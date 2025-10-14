79a30c3a5a9d546ebd916ab197d57a3a2adfab5c
oberdfr <alebennella@gmail.com>
2025-10-14 16:05:09 +0200

    drm: amdgpu: ps4_bridge: remove 1080p120 CEA mode
    This is no more needed because we will use high refresh resolution directly from the EDID.

--- drivers/gpu/drm/amd/amdgpu/ps4_bridge.c
+++ drivers/gpu/drm/amd/amdgpu/ps4_bridge.c
@@ -741,20 +741,6 @@ static const struct drm_display_mode mode_1080p = {
 		 DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_PVSYNC),
 	.picture_aspect_ratio = HDMI_PICTURE_ASPECT_16_9
 };
-/* Having a 120Hz modeline causes incorrect mode selection
- * by GUI / Display Manager, causing a 60Hz monitor to try
- * with a 120Hz mode - leading to a blackscreen.
- * Only fix seems to be having a xorg.conf in /usr/share/X11/xorg.conf.d/
- *
- * Try setting a TYPE_PREFFERED mode
- */
-/* 63 - 1920x1080@120Hz */
-static const struct drm_display_mode mode_1080p120 = {
-	DRM_MODE("1920x1080", DRM_MODE_TYPE_DRIVER, 297000, 1920, 2008,
-			2052, 2200, 0, 1080, 1084, 1089, 1125, 0,
-		   DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_PVSYNC),
-	  .picture_aspect_ratio = HDMI_PICTURE_ASPECT_16_9
-};
 
 int ps4_bridge_get_modes(struct drm_connector *connector)
 {
@@ -795,10 +781,6 @@ int ps4_bridge_get_modes(struct drm_connector *connector)
 
 	mode = drm_mode_duplicate(dev, &mode_1080p);
     mode->type |= DRM_MODE_TYPE_PREFERRED;
-	drm_mode_probed_add(connector, mode);
-    count++;
-
-	mode = drm_mode_duplicate(dev, &mode_1080p120);
 	drm_mode_probed_add(connector, mode);
     count++;
 
@@ -853,8 +835,7 @@ enum drm_mode_status ps4_bridge_mode_valid(struct drm_connector *connector,
 	int vic = drm_match_cea_mode(mode);
 
 	/* Directly allow anything that we can match up to a VIC (CEA modes) */
-	if ((vic == 16 && vic == 4 && vic == 1 && vic == 63)) {
-	// Might need to disable 63 (1920x1080-120Hz)
+	if ((vic == 16 && vic == 4 && vic == 1)) {
         DRM_DEBUG_KMS("[MODE_OK] Mode %dx%d@%d clock %d kHz match with VIC %d\n",
                   mode->hdisplay, mode->vdisplay,
                   drm_mode_vrefresh(mode), mode->clock, vic);
